d25aee1c6c403bf27fb85ff06991a5cd2c62707f
bubushkin <dibadog@gmail.com>
2024-01-01 19:12:47 -0500

    agent model

---
+++ src/Agent.cpp
@@ -0,0 +1,50 @@
+/*
+ * Agent.cpp
+ *
+ *  Created on: Jan 1, 2024
+ *      Author: iskandar
+ */
+
+#include "include/Agent.h"
+
+Agent::Agent(unsigned int id, std::string ip_address, std::string uuid, unsigned long last_contact){
+	this->_id = id;
+	this->_ip_address = ip_address;
+	this->_uuid = uuid;
+	this->_last_contact = last_contact;
+
+}
+
+std::string Agent::getIpAddress() const{
+	return this->_ip_address;
+
+}
+std::string Agent::getUUID() const {
+	this->_uuid;
+
+}
+unsigned long Agent::getLastContact() const {
+	this->_last_contact;
+}
+
+unsigned int Agent::getId() const {
+	this->_id;
+}
+
+
+void Agent::setId(const unsigned int id) {
+	this->_id = id;
+}
+
+
+void Agent::setIpAddress(std::string *pip_address) {
+	this->_ip_address = *pip_address;
+
+}
+void Agent::setUUID(std::string *puuid) {
+	this->_uuid = *puuid;
+
+}
+void Agent::setLastContact(unsigned long *plast_contact) {
+	this->_last_contact = *plast_contact;
+}
---
+++ src/include/Agent.h
@@ -0,0 +1,38 @@
+/*
+ * Agent.h
+ *
+ *  Created on: Jan 1, 2024
+ *      Author: iskandar
+ */
+
+#ifndef SRC_INCLUDE_AGENT_H_
+#define SRC_INCLUDE_AGENT_H_
+
+#include <string>
+
+class Agent {
+public:
+	Agent();
+	Agent(unsigned int, std::string, std::string, unsigned long);
+	virtual ~Agent();
+
+	std::string getIpAddress() const;
+	std::string getUUID() const;
+	unsigned long getLastContact() const;
+	unsigned int getId() const;
+
+	void setIpAddress(std::string *);
+	void setUUID(std::string *);
+	void setLastContact(unsigned long *);
+	void setId(const unsigned int);
+
+private:
+
+	unsigned int _id;
+	std::string _ip_address;
+	std::string _uuid;
+	unsigned long  _last_contact;
+
+};
+
+#endif /* SRC_INCLUDE_AGENT_H_ */
