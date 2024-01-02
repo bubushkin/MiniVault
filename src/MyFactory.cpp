e7602b7bd121729bb867347b6c88cb309925fe6e
bubushkin <dibadog@gmail.com>
2024-01-01 19:12:15 -0500

    added agent base model. query agent in db.

--- src/MyFactory.cpp
+++ src/MyFactory.cpp
@@ -4,6 +4,79 @@
 
 #include "include/MyFactory.h"
 
-myfactory::myfactory() {
 
+#define SQLEXCEPTION \
+		std::cout << "# ERR: SQLException in " << __FILE__; \
+		std::cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << std::endl; \
+		std::cout << "# ERR: " << ex.what(); \
+		std::cout << " (MySQL error code: " << ex.getErrorCode(); \
+		std::cout << ", SQLState: " << ex.getSQLState() << " )" << std::endl; \
+
+
+MyFactory::MyFactory(std::unordered_map<std::string, std::string> &pconfig) {
+
+
+	this->_prset = nullptr;
+	this->_pstmt = nullptr;
+	this->_schema = pconfig["schema"];
+	this->_user = pconfig["user"];
+	this->_password = pconfig["password"];
+
+	try{
+		this->_pdriver = sql::mysql::get_mysql_driver_instance();
+		this->_pconn = this->_pdriver->connect(this->_host, this->_user, this->_password);
+		this->_pconn->setSchema(pconfig["schema"]);
+	} catch(sql::SQLException &ex){
+		SQLEXCEPTION
+		exit(EXIT_FAILURE);
+	}
+}
+
+Agent *MyFactory::getAgent(std::string *pip_address){
+
+
+	Agent *pagent = nullptr;
+	std::string SQL = "SELECT id, ip_address, uuid, last_contact FROM " + this->_schema + " WHERE ip_address = ?";
+	sql::PreparedStatement *pstmt = nullptr;
+	sql::ResultSet *prset = nullptr;
+
+	try{
+		pstmt = this->_pconn->prepareStatement(SQL);
+		pstmt->setString(1, *pip_address);
+		prset = pstmt->executeQuery();
+		while(prset->next()){
+			pagent = new Agent(prset->getInt(1), prset->getString(2), prset->getString(3), prset->getUInt64(4));
+		}
+
+	} catch(sql::SQLException &ex){
+		SQLEXCEPTION
+	}
+
+	if(pstmt) delete pstmt;
+	if(prset) delete prset;
+	return pagent;
+
+}
+
+std::vector<Agent*>* MyFactory::getAgents(){
+	std::vector<Agent *> *pagents = new std::vector<Agent *>();
+
+	std::string SQL = "SELECT id, ip_address, uuid, last_contact FROM " + this->_schema;
+	sql::PreparedStatement *pstmt = nullptr;
+	sql::ResultSet *prset = nullptr;
+
+	try{
+		pstmt = this->_pconn->prepareStatement(SQL);
+		prset = pstmt->executeQuery();
+		while(prset->next()){
+			pagents->push_back(new Agent(prset->getInt(1), prset->getString(2), prset->getString(3), prset->getUInt64(4)));
+		}
+
+	} catch(sql::SQLException &ex){
+		SQLEXCEPTION
+	}
+
+	if(pstmt) delete pstmt;
+	if(prset) delete prset;
+	return pagents;
 }
--- src/include/MyFactory.h
+++ src/include/MyFactory.h
@@ -6,30 +6,38 @@
 #define MINIVAULT_MYFACTORY_H
 
 #include <string>
+#include <cstdlib>
 #include <mysql_driver.h>
 #include <mysql_connection.h>
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/resultset.h>
 #include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
 
-class myfactory {
+#include <vector>
+#include "Agent.h"
+
+class MyFactory {
 
 public:
-    myfactory();
-    void init_cnx();
+	MyFactory(std::unordered_map<std::string, std::string> &);
+
     const std::string *getHostname();
-    const std::string *getSchemaname();
+    const std::string *getSchema();
+    Agent *getAgent(std::string *);
+    std::vector<Agent*>*  getAgents();
 
 private:
-    std::string host;
-    std::string schema;
-    std::string username;
-    std::string password;
-    sql::mysql::MySQL_Driver *pdriver;
-    sql::Connection *pconn;
-    sql::Statement *pstmt;
-    sql::ResultSet *prset;
+    std::string _host;
+    std::string _schema;
+    std::string _user;
+    std::string _password;
+
+    sql::mysql::MySQL_Driver *_pdriver;
+    sql::Connection *_pconn;
+    sql::PreparedStatement *_pstmt;
+    sql::ResultSet *_prset;
 
 };
 
